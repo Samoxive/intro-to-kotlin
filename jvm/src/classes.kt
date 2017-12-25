39b4d085f06914d4c6975b2277989688a447f8c8
Şamil NART <samil.nart@gmail.com>
2017-12-25 22:27:54 +0300

    Add examples about classes

--- jvm/src/classes.kt
+++ jvm/src/classes.kt
@@ -1,7 +1,45 @@
-// We can specify a constructor right next to the class definition
-// this allows us to
-class Point(val x: Int, val y: Int)
+// This syntax is used to define classes, we can specify a constructor right next to the class name
+class Point(x: Int, y: Int) {
+    val x = x
+    val y = y
+}
+// We can also put fields right into the constructor (they are called properties in Kotlin),
+// this allows us to define class fields and assign them in constructor in the same time
+class Point1(val x: Int, val y: Int)
+// Above code's equivalent in Java:
+// class Point {
+//     public int x;
+//     public int y;
+//
+//     public Point(int x, int y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// We can create getters and setters for properties in Kotlin right next to the declarations.
+// When we define a setter, it will be called when the developer tries to modify it
+// `bag.items = listOf("Apple", "Banana")` the setter is going to be called with the value in right hand side of the equation
+// Same goes for getters, when we access the property like this: `println(bag.items)`, its getter will be called
+class Bag<T>(items: List<T>) {
+    var items = items
+        get() {
+            println("Someone touched the items in the bag!")
+            return field
+        }
+        set(value) {
+            field = value
+            println("Items in the bag changed!")
+        }
+}
+
 
 fun main(args: Array<String>) {
     val p = Point(1, 2)
+
+    val bag = Bag(listOf("Apple", "Banana"))
+    println(bag.items)
+
+    bag.items = listOf("Mango", "Kiwi")
+    println(bag.items)
 }
